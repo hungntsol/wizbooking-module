56d4f12b70e4476a5d1bbcd05b0f530db9949fd7
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-28 23:18:08 +0700

    [platform] [efcore] unit of work abstract

---
+++ src/Platform/EFCore.Persistence/Abstracts/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EFCore.Persistence.Abstracts;
+
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// Begin a transaction scope
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<IDbContextTransaction> StartTransaction(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Flush all saveChanges have been made in scope
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<IDbContextTransaction> CommitTransaction(CancellationToken cancellationToken = default);
+}
