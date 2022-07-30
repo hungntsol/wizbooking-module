fec921161daa60e64e7a8ddd54da107907178e2f
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-30 20:01:07 +0700

    [meeting] [infra] DI service

---
+++ src/Services/Meeting/Meeting.Infrastructure/DependencyInjection/InfrastructureServicesExtension.cs
@@ -0,0 +1,27 @@
+﻿using EFCore.Persistence.DependencyInjection;
+using Meeting.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Meeting.Infrastructure.DependencyInjection;
+public static class InfrastructureServicesExtension
+{
+    public static IServiceCollection AddInfrastructureService(this IServiceCollection services, 
+        ILogger logger,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<MeetingDataContext>(options =>
+        {
+            var connectionString = configuration.GetValue<string>("MeetingDataContext:ConnectionString");
+            logger.LogInformation("MeetingDb's connectionString: {Connection}", connectionString);
+
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddUnitOfWork<MeetingDataContext>();
+
+        return services;
+    }
+}
