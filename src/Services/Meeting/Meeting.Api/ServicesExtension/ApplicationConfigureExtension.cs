c59b46a442dc84b161116f5b20946cfa08acebef
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-30 19:30:33 +0700

    [meeting] [domain] create entity

---
+++ src/Services/Meeting/Meeting.Api/ServicesExtension/ApplicationConfigureExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace Meeting.Api.ServicesExtension;
+
+public static class ApplicationConfigureExtension
+{
+    public static IServiceCollection Setup(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add services to the container.
+
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication Configure(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
---
+++ src/Services/Meeting/Meeting.Domain/Entities/ProviderUrl.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedCommon.Domain;
+
+namespace Meeting.Domain.Entities;
+
+[Index(nameof(Url), Name = "IX_ProviderUrl", IsUnique = true)]
+public class ProviderUrl : EntityBase<ulong>
+{
+    public string Url { get; set; } = null!;
+    public int CreatorId { get; set; }
+    public virtual AppUser Creator { get; set; } = null!;
+    public DateTime ExpiredAt { get; set; }
+    public string AllowServicesPattern { get; set; } = "*";
+    public string? DenyServicesPattern { get; set; }
+    public bool IsActive { get; set; }
+
+    public ProviderUrl(string url,
+        int creatorId,
+        DateTime expiredAt,
+        string allowServicesPattern,
+        string? denyServicesPattern,
+        bool isActive)
+    {
+        Url = url;
+        CreatorId = creatorId;
+        ExpiredAt = expiredAt;
+        AllowServicesPattern = allowServicesPattern;
+        DenyServicesPattern = denyServicesPattern;
+        IsActive = isActive;
+    }
+}
