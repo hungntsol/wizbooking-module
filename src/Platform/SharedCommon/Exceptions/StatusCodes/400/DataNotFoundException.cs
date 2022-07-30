87ca63b11a303def43c257bdeb0cb0b66d6b17c5
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-31 00:48:47 +0700

    [platform] define http exception

---
+++ src/Platform/SharedCommon/Exceptions/StatusCodes/400/DataNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedCommon.Exceptions.StatusCodes._400;
+public class DataNotFoundException : HttpBaseException
+{
+    public DataNotFoundException() : base(400, "Data not found")
+    {
+
+    }
+
+    public DataNotFoundException(string message) : base(400, message)
+    {
+
+    }
+}
