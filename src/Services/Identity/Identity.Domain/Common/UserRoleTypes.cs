2118bb6ad7b2fbf261e1336ddd8ef2998a4c1c6e
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-31 10:54:17 +0700

    [identity] migrate v1.0

---
+++ src/Services/Identity/Identity.Domain/Common/UserRoleTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace Identity.Domain.Common;
+
+public static class UserRoleTypes
+{
+    public const string NORMAL = "NORMAL";
+    public const string MODERATOR = "MODERATOR";
+    public const string ADMIN = "ADMIN";
+
+    public const string DEFAULT = NORMAL;
+
+    private static readonly List<string> _roles = new() { UserRoleTypes.NORMAL, UserRoleTypes.MODERATOR, UserRoleTypes.ADMIN };
+
+    public static bool IsDefined(string role)
+    {
+        return _roles.Contains(role);
+    }
+}
